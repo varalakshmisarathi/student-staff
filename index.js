4a8f7d580b6a1eeee68821ba270c3c46cc9d6dda
varalakshmi <lakshmisarathi08@gmail.com>
2020-07-10 20:26:20 +0000

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(bodyparser.json());
 let staffDetail = [
