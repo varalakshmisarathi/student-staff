3928ba3f15e4dd79c5ff79f2ed291aaf55c5d0a6
varalakshmi <lakshmisarathi08@gmail.com>
2020-07-10 20:12:31 +0000

    node

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
-const port = 5000;
+const port = 4000;
 
 app.use(bodyparser.json());
-let staffDetail = []
-let studentDetail = []
+let staffDetail = [
+  {
+    "id":1,
+    "name":"staff1"
+},
+{
+    "id":2,
+    "name":"staff2"
+}
+]
+let studentDetail = [
+  {
+    "id":1,
+    "name":"lakshmi",
+    "staffid":1
+},
+{
+    "id":2,
+    "name":"varalakshmi",
+    "staffid":"2"
+}
+]
 app.post("/studentDetail", (req, res) => {
   studentDetail.push(req.body);
-  res.json({ message: "studentCreation created successfully!" })
+  res.json({ message: "studentdetail created successfully!" })
 });
 app.get("/studentDetail", (req, res) => {
   res.send(studentDetail)
 })
 app.post("/staffDetail", (req, res) => {
   staffDetail.push(req.body);
-  res.json({ message: "staffCreation created successfully!" })
+  res.json({ message: "staffdetail created successfully!" })
 });
 
-app.get("/getstaffdetail", (req, res) => {
+app.get("/staffdetail", (req, res) => {
   let staff = staffDetail.map((data) => {
-    let count = studentDetail.filter((item) => item.staffid === data.id);
+    let count = studentDetail.filter((entry) => entry.staffid === data.id);
     return {
       id: data.id,
       name: data.name,
@@ -41,14 +61,14 @@ app.put("/studentDetail/:id", (req, res) => {
   })
 })
 
-app.delete("/remove/:id", (req, res) => {
-  let filterval = studentDetail.filter((element) => {
+app.delete("/delete/:id", (req, res) => {
+  let filtervalue = studentDetail.filter((element) => {
     if (element.id == req.params.id) {
       return element;
     }
   })[0];
 
-  studentDetail = filterval;
+  studentDetail = filtervalue;
   res.send(studentDetail);
 });
 app.listen(process.env.PORT || port, () => {
